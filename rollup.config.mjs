0eb54ea7fc3b870a15cebc5b662d9773d154f82d
NicoleOkamoto <nicoleokamoto@icloud.com>
2024-06-27 11:36:36 -0500

    added css tested -works

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,11 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+
+
 import packageJson from "./package.json" assert { type: "json" };
+
 
 // const packageJson = require("./package.json");
 
@@ -25,11 +29,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
