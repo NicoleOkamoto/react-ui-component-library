ef311ea6338310fbdb31f4e657c3bca5c0bc3b53
NicoleOkamoto <nicoleokamoto@icloud.com>
2024-07-03 01:53:04 -0500

    Included test for text component

--- src/components/Text/Text.test.jsx
+++ src/components/Text/Text.test.jsx
@@ -10,4 +10,22 @@ describe('Text', () => {
     const textElement = screen.getByText(/React has become a top choice for/i);
     expect(textElement).toBeInTheDocument();
   });
+
+  test('Text component change background color when disabled and cursor is not allowed', () => {
+    const { getByText } = render(
+      <Text
+        color="black"
+        disabled
+        fontSize="16px"
+        text="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        textAlign="justify"
+      />,
+    );
+    const textElement = getByText(
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    );
+    expect(textElement).toBeVisible();
+    expect(textElement).toHaveStyle('cursor: not-allowed');
+    expect(textElement).toHaveStyle('opacity: 0.6');
+  });
 });
