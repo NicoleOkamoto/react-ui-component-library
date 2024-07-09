08139f1f8dffeb6c760d49a56e571f2f017c54b7
NicoleOkamoto <nicoleokamoto@icloud.com>
2024-07-08 19:42:20 -0500

    Updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 
 export default [
-  { ignores: ["node_modules/", "storybook-static/", "**/*.d.ts", 'dist/'] },
-  { files: ["**/*.{mjs,cjs,ts,jsx,tsx}"] },
+  { ignores: ['node_modules/', 'storybook-static/', '**/*.d.ts', 'dist/'] },
+  { files: ['**/*.{mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   ...tseslint.configs.recommended,
   pluginReactConfig,
-
 ];
--- storybook-static/sb-addons/interactions-11/manager-bundle.js
+++ storybook-static/sb-addons/interactions-11/manager-bundle.js
@@ -13207,16 +13207,14 @@ In order to be iterable, non-array objects must have a [Symbol.iterator]() metho
             { style: { color: n.base } },
             '[\u2026]',
           );
-        let o = e
-            .slice(0, 3)
-            .map((i) =>
-              y.createElement(bt, {
-                key: JSON.stringify(i),
-                value: i,
-                nested: !0,
-                callsById: r,
-              }),
-            ),
+        let o = e.slice(0, 3).map((i) =>
+            y.createElement(bt, {
+              key: JSON.stringify(i),
+              value: i,
+              nested: !0,
+              callsById: r,
+            }),
+          ),
           a = Pd(o, y.createElement('span', null, ', '));
         return e.length <= 3
           ? y.createElement('span', { style: { color: n.base } }, '[', a, ']')
